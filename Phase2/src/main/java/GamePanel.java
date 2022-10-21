f9aa32bacf82921da2a4c0bda90edcbb0b352119
crtung <connor_tung@sfu.ca>
2022-10-21 15:12:46 -0700

    Created the window and game panel

---
+++ Phase2/src/main/java/GamePanel.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    final int originalTileSize = 16; //16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; //48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; //768 pixels
+    final int screenHeight = tileSize * maxScreenRow; //576 pixels
+
+    Thread gameThread;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight)); //setting the panel size
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this); // starts the thread for the game
+        gameThread.start();
+    }
+
+    @Override
+    public void run(){
+        //where the core of the game will go
+    }
+
+
+}
