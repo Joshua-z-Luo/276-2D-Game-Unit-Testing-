003d2df96dd6aaa9b34b1e9bb04031b1254f90b0
crtung <connor_tung@sfu.ca>
2022-10-21 21:50:30 -0700

    Added movement functionality

--- Phase2/src/main/java/GamePanel.java
+++ Phase2/src/main/java/GamePanel.java
@@ -11,12 +11,20 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; //768 pixels
     final int screenHeight = tileSize * maxScreenRow; //576 pixels
 
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    //set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight)); //setting the panel size
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -27,6 +35,54 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run(){
         //where the core of the game will go
+        double drawInterval = 1000000000/60;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while(gameThread != null){
+            //updates character position
+            update();
+            //draws the screen with updated info
+            repaint();
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+                if(remainingTime < 0){
+                    remainingTime = 0;
+                }
+                Thread.sleep((long) remainingTime);
+                nextDrawTime += drawInterval;
+
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void update(){
+        if(keyH.upPressed){
+            playerY -= playerSpeed;
+        }
+        else if(keyH.leftPressed){
+            playerX -= playerSpeed;
+        }
+        else if(keyH.rightPressed){
+            playerX += playerSpeed;
+        }
+        else if(keyH.downPressed){
+            playerY += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
     }
 
 
