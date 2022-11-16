f1688696f1abf24e2c3f90aaea62469f0af09411
crtung <connor_tung@sfu.ca>
2022-11-15 16:54:51 -0800

    Added new test

--- Phase2/src/test/java/Entities/MainCharacterTVTest.java
+++ Phase2/src/test/java/Entities/MainCharacterTVTest.java
@@ -1,5 +1,6 @@
 package Entities;
 
+import Entities.object.OBJ_Battery;
 import main.GamePanel;
 import main.KeyHandler;
 import org.junit.jupiter.api.Test;
@@ -21,6 +22,18 @@ class MainCharacterTVTest {
        for(int i = 0;i<999;i++){
            mC.interactMonster(i);
            assertEquals(0,mC.life);
+       }
+   }
+
+   @Test
+    void lifeIncreasesWhenPowerUpCollected(){
+       int score = mC.score;
+       for(int i = 0; i < gp.obj.length; i++){
+           if(gp.obj[i] != null && gp.obj[i].getClass().equals(OBJ_Battery.class)){
+               score+= 100;
+               mC.pickUpObject(i);
+               assertEquals(score, mC.score);
+           }
        }
    }
 }
