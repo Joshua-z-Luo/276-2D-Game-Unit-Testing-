1f4decb8ef379e253834383843c49501aa940d1c
crtung <connor_tung@sfu.ca>
2022-11-15 17:19:00 -0800

    Fixed tests

--- Phase2/src/test/java/Entities/MainCharacterTVTest.java
+++ Phase2/src/test/java/Entities/MainCharacterTVTest.java
@@ -2,8 +2,10 @@ package Entities;
 
 import Entities.object.OBJ_Battery;
 import Entities.object.OBJ_Hole;
+import Entities.object.OBJ_KeyCard;
 import main.GamePanel;
 import main.KeyHandler;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,24 +16,38 @@ class MainCharacterTVTest {
     public static KeyHandler kH = new KeyHandler(gp);
     public static MainCharacterTV mC = new MainCharacterTV(gp,kH);
 
-//   @BeforeAll
-//   static void init() {
-//   }
+   @BeforeAll
+   static void init() {
+       gp.setUpGame();
+       gp.startGameThread();
+   }
 
    @Test
     void lifeGoesZeroWhenMonsterIndexIsNot999(){
-       for(int i = 0;i<999;i++){
+       for(int i = 0;i < 999;i++){
            mC.interactMonster(i);
            assertEquals(0,mC.life);
        }
    }
 
    @Test
-    void lifeIncreasesWhenPowerUpCollected(){
+    void scoreIncreasesWhenPowerUpCollected(){
        int score = mC.score;
        for(int i = 0; i < gp.obj.length; i++){
            if(gp.obj[i] != null && gp.obj[i].getClass().equals(OBJ_Battery.class)){
                score+= 100;
+               mC.pickUpObject(i);
+           }
+           assertEquals(score, mC.score);
+       }
+   }
+
+   @Test
+    void scoreIncreasesWhenKeyCardCollected(){
+       int score = mC.score;
+       for(int i = 0; i < gp.obj.length; i++){
+           if(gp.obj[i] != null && gp.obj[i].getClass().equals(OBJ_KeyCard.class)){
+               score += 200;
                mC.pickUpObject(i);
            }
            assertEquals(score, mC.score);
