916127647a212fe23f56bfeb5653c4b7ec3babc8
Rose Epstein <rea13@sfu.ca>
2022-11-15 18:01:00 -0800

    Added simulated S key being pressed to MainCharacterMovesDownWhenDownKeyPressed() method in KeyhandlerTest.

--- Phase2/src/test/java/main/KeyHandlerTest.java
+++ Phase2/src/test/java/main/KeyHandlerTest.java
@@ -7,20 +7,28 @@ import main.UI;
 import org.junit.jupiter.api.Test;
 
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class KeyHandlerTest {
     public static boolean upPressed, downPressed, leftPressed, rightPressed; // do we need?
+    KeyEvent e;
     public static GamePanel gp = new GamePanel();
     public static KeyHandler kH = new KeyHandler(gp);
-    public static MainCharacterTV mC = new MainCharacterTV(gp,kH);
+    public static MainCharacterTV tvGuy = new MainCharacterTV(gp,kH);
+
+    public void keyTyped(KeyEvent e) {}
 
     @Test
     void MainCharacterMovesDownWhenDownKeyPressed(){
-        // fill in
-
+        // simulate key being pressed
+        KeyEvent key = new KeyEvent(gp, KeyEvent.KEY_PRESSED, System.currentTimeMillis(),
+                0, KeyEvent.VK_S, 'S');
+        gp.getKeyListeners()[0].keyPressed(key);
+        // check if direction is down
+        assertEquals("down",tvGuy.direction);
     }
     @Test
     void MainCharacterMovesUpWhenUpKeyPressed(){
